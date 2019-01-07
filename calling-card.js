a3cdb74e21517c8968688be30408df614002634a
Jason Smith <jason.smith@infusionsoft.com>
2019-01-06 19:44:03 -0700

    OOP Checkpoint

--- calling-card.js
+++ calling-card.js
@@ -1,3 +1,32 @@
 class CallingCard {
+    constructor(costPerMinute) {
+       this.costPerMinute = costPerMinute;
+       this.minutesRemaining = 0;
+      }
+
+      addDollars(amountToAdd){
+          let cents = amountToAdd * 100;
+          let minutes = cents / this.costPerMinute;
+
+          if(minutes % 2 === 0){
+              this.minutesRemaining = minutes;
+          }else{
+              Math.floor(minutes);
+          }
+      }
+
+      getRemainingMinutes(){
+        return this.minutesRemaining;
+      }
+
+      useMinutes(callLengthInMin){
+        if( this.minutesRemaining - callLengthInMin < 0){
+            this.minutesRemaining = 0;
+        }else{
+           this.getRemainingMinutes -= callLengthInMin;
+        }
+      }
+    
+      
     
 }
--- cell-phone.js
+++ cell-phone.js
@@ -1,3 +1,55 @@
 class CellPhone {
-    
+    constructor(callingCard){
+        this.callingCard = callingCard;
+        this.isTalking = false;
+        this.currentCall= {
+            lengthOfCall: 0,
+            phoneNumber: '',
+            callCutOff: false,
+        }
+
+        this.previousCalls = [];
+    }
+
+    call(phoneNumber){
+        this.currentCall.lengthOfCall = 0;
+        this.currentCall.phoneNumber = phoneNumber;
+        this.currentCall.callCutOff = false;
+    }
+
+    isTalking(){
+        return this.currentCall.lengthOfCall > 0 ? true : false;
+    }
+
+    tick(){
+        this.currentCall.lengthOfCall++;
+        this.callingCard.minutesRemaining--;
+
+        if(this.callingCard.minutesRemaining === 0) {
+            this.currentCall.callCutOff = true;
+            this.endCall();
+        }
+        
+    }
+
+    endCall(){
+        this.previousCalls.push(this.currentCall);
+        this.currentCall = {};
+
+    }
+
+    getHistory(){
+        let callHistory = "";
+
+        this.previousCalls.forEach(phoneCall => {
+            let {phoneNumber, lengthOfCall, callCutOff} = phoneCall;
+            let singularOrPlural = lengthOfCall <= 1 ? 'minute' : 'minutes';
+            let callWasCutOff = callCutOff ? 'cut off at' : '';
+
+            callHistory += `${callWasCutOff} ${phoneNumber} (${lengthOfCall} ${singularOrPlural}),`
+        });
+
+        return callHistory.slice(0, callHistory.length-1);
+
+    }
 }
